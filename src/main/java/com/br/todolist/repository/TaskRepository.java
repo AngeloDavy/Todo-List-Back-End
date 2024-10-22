b6b4cf64142059ca1cefd4aeadf9be94eb60510f
angelo <angeloportela2@gmail.com>
2024-10-21 23:08:57 -0300

    feat

--- src/main/java/com/br/todolist/repository/TaskRepository.java
+++ src/main/java/com/br/todolist/repository/TaskRepository.java
@@ -1,4 +1,10 @@
 package com.br.todolist.repository;
 
-public interface TaskRepository {
+
+import com.br.todolist.model.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
 }
